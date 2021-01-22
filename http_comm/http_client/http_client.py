b80bfec7f1247114c62a8510d8ebb5e65da3d196
Enzo <enzocalvino3@gmail.com>
2021-01-22 12:35:53 +0100

    dev-class-http_client-structure

---
+++ http_comm/http_client/http_client.py
@@ -0,0 +1,21 @@
+# class for http client
+
+import http.client
+
+class http_client():
+	
+	"""docstring for http_client"""
+	def __init__(self, ip, port, ip_server, ip_port):
+		self.ip = ip
+		self.port = port
+		self.ip_server = server
+		self.ip_port = ip_port
+
+	"""establish a connection wuth the server"""
+	def connect():
+		pass
+
+	""" make http requests (GET, POST)"""
+	def request():
+		pass
+
