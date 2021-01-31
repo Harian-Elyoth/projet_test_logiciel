42095b66654759b5716f6e52e55f1099709288d6
Enzo <enzocalvino3@gmail.com>
2021-01-31 16:07:48 +0100

    test-new-http_server-none_server_launch

--- http_comm/http_server/test_http_server.py
+++ http_comm/http_server/test_http_server.py
@@ -1,7 +1,11 @@
 # test class for http server
 
 import unittest
+
 from http_server import *
+
+# library for launch requests on a server
+import requests 			
 
 class test_http_server(unittest.TestCase):
 
@@ -9,44 +13,60 @@ class test_http_server(unittest.TestCase):
 	def setUp(self):
 		pass
 
-	# ---------------------- #
-	# TEST __INIT__ FUNCTION #
-	# ---------------------- #
+	# --------------------- #
+	# TEST __NEW__ FUNCTION #
+	# --------------------- #
+
+	# everything's fine
+	def test_new(self):
+		# THREAD OU MOCK
+		# self.assertEqual(http_server("127.0.0.1", 65500), 0)
+		pass
 
 	# ip is a string
 	def test_ip_type(self):
-		pass
+		self.assertEqual(http_server(65500, 65500), -1)
 
 	# ip is something like 192.168.41.2
 	def test_ip_format(self):
-		pass
+		self.assertEqual(http_server("127.899.320.490", 65500), -2)
 
 	# port is an integer
 	def test_port_type(self):
-		pass
+		self.assertEqual(http_server("127.0.0.1", "65500"), -3)
 
 	# port is between 0 and 65535
 	def test_port_format(self):
-		pass
+		self.assertEqual(http_server("127.0.0.1",   -15), -4)
+		self.assertEqual(http_server("127.0.0.1", 65800), -4)
 
 	# port is available : 49152 - 65535
 	def test_port_allow(self):
-		pass
+		self.assertEqual(http_server("127.0.0.1",    15), -4)
+		self.assertEqual(http_server("127.0.0.1", 65800), -4)
 
 	# -------------------- #
 	# TEST DO_GET FUNCTION #
 	# -------------------- #
 
-	# do a GET on the server
-	def test_get():
+	# do a GET on the server, everything's fine
+	def test_get(self):
+		pass
+
+	# do a GET on the server, endpoint not found
+	def test_get_not_found(self):
 		pass
 
 	# --------------------- #
 	# TEST DO_POST FUNCTION #
 	# --------------------- #
 
-	# do a POST on the server
-	def test_post():
+	# do a POST on the server, everything's fine
+	def test_post(self):
+		pass
+
+	# do a POST on the server, endpoint not found
+	def test_post_not_found(self):
 		pass
 
 if __name__ == '__main__':
