4ae9b097d01ab36d0a135b1ee86616af2e5d3feb
Enzo <enzocalvino3@gmail.com>
2021-02-02 19:31:07 +0100

    dev-http_server-update_init_do_GET_&_do_POST

--- http_comm/http_server/http_server.py
+++ http_comm/http_server/http_server.py
@@ -3,28 +3,48 @@
 import http.server
 
 class handler_http_serv(http.server.BaseHTTPRequestHandler):
+	
+	"""docstring for handler_http_serv class"""
+
 	def __init__(self, *args, **kwargs):
 		super(handler_http_serv, self).__init__(*args, **kwargs)
 			
-	# handle GET request
+	"""handle GET request"""
 	def do_GET(self):
-		self.send_response(200)
-		self.send_header("Content-type", "text/html")
-		self.end_headers()
+
+		# print("do_GET : path = " + self.path)
+
+		if self.path == '/':
+			self.send_response(200)
+			self.send_header("Content-type", "text/plain")
+			self.end_headers()
+		else:
+			self.send_response(404)
+			self.send_header("Content-type", "text/plain")
+			self.end_headers()
+
 		# pass
 
-	# handle POST request
+	"""handle POST request"""
 	def do_POST(self):
-		self.send_response(200)
-		self.send_header("Content-type", "text/html")
-		self.end_headers()
+
+		# print("do_POST : path = " + self.path)
+
+		if self.path == '/':
+			self.send_response(200)
+			self.send_header("Content-type", "text/plain")
+			self.end_headers()
+		else:
+			self.send_response(404)
+			self.send_header("Content-type", "text/plain")
+			self.end_headers()
 		# pass
 
 class http_server(object):
 
-	"""docstring for http_server"""
+	"""docstring for http_server class"""
 
-	# called when creating instance, return an object (can be an integer)
+	"""called when creating instance, return an object (can be an integer)"""
 	def __new__(self, ip, port):
 
 		# TESTS on parameters
@@ -59,29 +79,32 @@ class http_server(object):
 
 		return super(http_server, self).__new__(self)
 
-	# called for initialize object (after new), return None
+	"""called for initialize object (after new), return None"""
 	def __init__(self, ip, port):
 		
-		# super(http_server, self).__init__(self)
-
 		self.ip 		= ip
 		self.port 		= port
 
 		try:
-			self.http_serv 	= http.server.HTTPServer((self.ip, self.port), handler_http_serv)
+			self.http_serv 	= http.server.ThreadingHTTPServer((self.ip, self.port), handler_http_serv)
 		except:
-			pass
+			print("Exception in http.server.ThreadingHTTPServer(..)")
 
 		# try:
-		# 	self.http_serv.serve_forever()
+		# 	http_serv.serve_forever()
 		# except KeyboardInterrupt:
-		# 	pass
+		# 	print("Exception in serve_forever()")
 		
-	# called when there not references anymore to close the server
+	"""called when there not references anymore to close the server"""
 	def __del__(self):
-		# self.http_serv.server_close()
+		self.http_serv.server_close()
 		pass
 
+if __name__ == '__main__':
 
-# serv_test = http_server("127.0.0.1", 65500)
-# serv_test_2 = http_server("127.899.320.490", 65500)
+	serv_test = http_server("127.0.0.1", 60000)
+
+	try:
+		serv_test.http_serv.serve_forever()
+	except KeyboardInterrupt:
+		print("Exception in serve_forever()")
