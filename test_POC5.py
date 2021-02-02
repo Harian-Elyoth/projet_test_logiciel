6462e4b51c15f8fdd63e237b544b15dc84b17f94
ml2405 <laghameriem24@gmail.com>
2021-02-02 09:45:41 +0100

    test-ajout_test_print_public_room

--- test_POC5.py
+++ test_POC5.py
@@ -8,7 +8,6 @@ from POC5 import connect_user
 from POC5 import exit
 #ROOM RELATED
 from POC5 import print_public_room
-from POC5 import print_public_room
 
 
 
@@ -30,11 +29,11 @@ class TestQuickToolsMethods(unittest.TestCase):
 
     def test_connect_user(self):
         #si l'utilisateur a tapé "login" alors la fonction doit renvoyer assertTrue
-        self.assertTrue(create_user("login"))
+        self.assertTrue(connect_user("login"))
         # si l'utilisateur a tapé "log" alors la fonction doit renvoyer False
-        self.assertFalse(create_user("log"))
+        self.assertFalse(connect_user("log"))
         # si l'utilisateur a tapé "login" suivi d'un autre mot alors la fonction doit renvoyer False
-        self.assertFalse(create_user("login bin"))
+        self.assertFalse(connect_user("login bin"))
 
     def test_exit(self):
         # si l'utilisateur a tapé "exit" alors la fonction doit renvoyer True
@@ -43,5 +42,14 @@ class TestQuickToolsMethods(unittest.TestCase):
         self.assertFalse(exit("xit"))
         # si l'utilisateur a tapé "exit" suivi d'un autre mot alors la fonction doit renvoyer False
         self.assertFalse(exit("exit room"))
+
+    def test_print_public_room(self):
+        # si l'utilisateur a tapé "public" alors la fonction doit renvoyer True
+        self.assertTrue(print_public_room("public"))
+        # si l'utilisateur a fait une faute de frappe alors la fonction doit renvoyer False
+        self.assertFalse(print_public_room("pblic"))
+        # si l'utilisateur a tapé "exit" suivi d'un autre mot alors la fonction doit renvoyer False
+        self.assertFalse(print_public_room("public room"))
+
 
         
