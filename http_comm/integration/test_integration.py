6687d79a2e5bb443abc46248d7c898ae80ef5454
azizidomar <azizidomar@etu.upmc.fr>
2021-02-05 13:14:45 +0100

    test-test_integration-strcture

---
+++ http_comm/integration/test_integration.py
@@ -0,0 +1,31 @@
+# test class for integration
+
+import unittest
+
+class test_integration(unittest.TestCase):
+
+	# ---------------------------- #
+	# INTEGRATION TEST GET REQUEST #
+	# ---------------------------- #
+
+	def test_get_request(self):
+		pass
+
+	# do a GET on the server, endpoint not found
+	def test_get_not_found(self):
+		pass
+
+	# ---------------------------- #
+	# INTEGRATION TEST GET REQUEST #
+	# ---------------------------- #
+
+	# everything's fine
+	def test_post_request(self):
+		pass
+
+	# everything's fine
+	def test_post_not_found(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
