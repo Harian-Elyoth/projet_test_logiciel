5b579bec641361fe2ca639175fcda40adf9e4b14
azizidomar <azizidomar@etu.upmc.fr>
2021-02-05 11:14:37 +0100

    update-test_handler-add-endpoint-management

--- http_comm/http_client/script_test.py
+++ http_comm/http_client/script_test.py
@@ -5,21 +5,33 @@ from io import BytesIO
 
 # Class for testing http_client
 class test_handler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		if self.path == '/':
+			self.send_response(200)
+			self.end_headers()
+			self.wfile.write(b'test : OK')
 
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'test : KO')
+		else:
+			self.send_response(404)
+			self.end_headers()
+			self.wfile.write(b'test : KO')
 
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        self.send_response(200)
-        self.end_headers()
+	def do_POST(self):
+		content_length = int(self.headers['Content-Length'])
+		body = self.rfile.read(content_length)
 
-        response = BytesIO()
-        response.write(body)
-        self.wfile.write(response.getvalue())
+		response = BytesIO()
+		response.write(body)
+
+		if self.path == '/':
+			self.send_response(200)
+			self.end_headers()
+			self.wfile.write(response.getvalue())
+
+		else:
+			self.send_response(404)
+			self.end_headers()
+			self.wfile.write(b'test : KO')
 
 if __name__ == '__main__':
 	httpd = HTTPServer(("127.0.0.1", 65501), test_handler)
