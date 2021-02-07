8828025de12cabf1220dbeb07dcc0288602a2378
Enzo <enzocalvino3@gmail.com>
2021-02-07 21:00:43 +0100

    dev-script_test_server-demo

---
+++ http_sql_socket/script_test_server.py
@@ -0,0 +1,20 @@
+# script for lunching sever
+
+import time
+
+from http_server import http_server
+from socket_comm import socket_comm
+
+if __name__ == '__main__':
+
+	print("Creation du websocket...\n")
+	server_socket = socket_comm("127.0.0.1", 60000)
+	server_socket.listen(5)
+	time.sleep(3)
+	server_socket.connect("127.0.0.1", 60001)
+
+	print("Creation du Serveur HTTP...\n")
+	serv_test = http_server("127.0.0.1", 60000)
+
+	print("Lancement du Serveur HTTP...\n")
+	serv_test.run()
