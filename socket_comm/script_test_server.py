9fd2a3542be684f1282952fa156075f849c46104
azizidomar <azizidomar@etu.upmc.fr>
2021-02-07 18:07:09 +0100

    update-script_tests

--- socket_comm/script_test_server.py
+++ socket_comm/script_test_server.py
@@ -1,17 +1,17 @@
 from socket_comm import socket_comm
 import time
 
-server_socket = socket_comm("127.0.0.1", 50000)
+server_socket = socket_comm("127.0.0.1", 60001)
 server_socket.listen(5)
-time.sleep(5)
+time.sleep(3)
 
-server_socket.connect("127.0.0.1", 50001)
-time.sleep(1)
+server_socket.connect("127.0.0.1", 60000)
+time.sleep(.5)
 
 server_socket.send_message("Hello client!")
-time.sleep(.1)
+time.sleep(.5)
 
 server_socket.send_message("Hello again client!")
-time.sleep(.1)
+time.sleep(.5)
 
 server_socket.stop_threads = True
