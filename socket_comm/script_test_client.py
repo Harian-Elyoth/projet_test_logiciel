4a6cb766a3b5f6cbdf0b1a30ea591ea7a4058397
azizidomar <azizidomar@etu.upmc.fr>
2021-02-07 17:32:01 +0100

    test-script_test_client-adding_test_script

---
+++ socket_comm/script_test_client.py
@@ -0,0 +1,17 @@
+from socket_comm import socket_comm
+import time
+
+client_socket = socket_comm("127.0.0.1", 50000)
+client_socket.listen(5)
+time.sleep(1)
+
+client_socket.connect("127.0.0.1", 50001)
+time.sleep(1)
+
+client_socket.send_message("Hello client!")
+time.sleep(.1)
+
+client_socket.send_message("Hello again client!")
+time.sleep(.1)
+
+client_socket.stop_threads = True
