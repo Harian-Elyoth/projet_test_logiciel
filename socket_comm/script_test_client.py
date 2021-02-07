82bf0f9a1ed9c96940afe4c9da124506a557896c
azizidomar <azizidomar@etu.upmc.fr>
2021-02-07 17:34:59 +0100

    update-script_test_client-change_port

--- socket_comm/script_test_client.py
+++ socket_comm/script_test_client.py
@@ -1,11 +1,11 @@
 from socket_comm import socket_comm
 import time
 
-client_socket = socket_comm("127.0.0.1", 50000)
+client_socket = socket_comm("127.0.0.1", 50001)
 client_socket.listen(5)
 time.sleep(1)
 
-client_socket.connect("127.0.0.1", 50001)
+client_socket.connect("127.0.0.1", 50000)
 time.sleep(1)
 
 client_socket.send_message("Hello client!")
