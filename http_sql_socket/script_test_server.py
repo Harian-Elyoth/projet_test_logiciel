f43cfed33745749d945b7278ae788b70727ef32f
Enzo <enzocalvino3@gmail.com>
2021-02-08 16:45:06 +0100

    update-script_test_server-changing_ports

--- http_sql_socket/script_test_server.py
+++ http_sql_socket/script_test_server.py
@@ -7,14 +7,18 @@ from socket_comm import socket_comm
 
 if __name__ == '__main__':
 
-	print("Creation du websocket...\n")
-	server_socket = socket_comm("127.0.0.1", 60000)
-	server_socket.listen(5)
-	time.sleep(3)
-	server_socket.connect("127.0.0.1", 60001)
-
 	print("Creation du Serveur HTTP...\n")
 	serv_test = http_server("127.0.0.1", 60000)
 
+	print("Creation du websocket...\n")
+	server_socket = socket_comm("127.0.0.1", 60001)
+	server_socket.listen(5)
+	time.sleep(3)
+	server_socket.connect("127.0.0.1", 60002)
+	server_socket.connect("127.0.0.1", 60003)
+
 	print("Lancement du Serveur HTTP...\n")
 	serv_test.run()
+
+	time.sleep(2)
+	server_socket.stop_threads = True
