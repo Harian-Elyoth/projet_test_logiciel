f96a6e4345172ab669690415146cd51d235364c8
ml2405 <laghameriem24@gmail.com>
2021-02-09 10:48:16 +0100

    Dev-ajout-connect_user

--- POC5.py
+++ POC5.py
@@ -5,17 +5,30 @@ from string import printable
 
 def create_user(motcle):
     if (len(motcle) != 6):
+        print("votre mot n'est pas de la bonne taille")
         return False
     elif ( isinstance(motcle, str) == False  ):
+        print("votre mot cle n'est pas un string")
         return False
     elif motcle == "create" :
+        print("C'est exact !")
         return True
     else:
         return False
 
 
 def connect_user(motcle):
-    return True
+    if (len(motcle) != 5):
+        print("votre mot n'est pas de la bonne taille")
+        return False
+    elif ( isinstance(motcle, str) == False ):
+        print("votre mot cle n'est pas un string")
+        return False
+    elif motcle == "login" :
+        print("C'est exact !")
+        return True
+    else:
+        return False
 
 def exit(motcle):
     return True
@@ -43,10 +56,22 @@ def send_to(motcle):
 
 
 def main():
-    print("Pour creer un user, tapez create")
-    s = input()
-    print(s)
-    print(create_user(s))
+    print("################################")
+    print("         Bienvenue              ")
+    print("################################")
+    print("")
+    #create user
+    ret = False
+    ret2 = False
+    while(ret == False):
+        print("Pour creer un user, tapez create")
+        s = input()
+        ret = create_user(s)
+
+    while(ret2 == False):
+        print("Pour se logger, tapez login")
+        s = input()
+        ret2 = connect_user(s)
 
 if __name__ == '__main__':
     main()
