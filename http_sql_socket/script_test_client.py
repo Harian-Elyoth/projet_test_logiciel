36e6763f74640863aea6e52bd2bc5bb3d4e24262
azizidomar <azizidomar@etu.upmc.fr>
2021-02-09 17:04:05 +0100

    update-script_test

--- http_sql_socket/script_test_client.py
+++ http_sql_socket/script_test_client.py
@@ -1,4 +1,4 @@
-import time
+import time, os
 
 from http_client import http_client
 from socket_comm import socket_comm
@@ -13,151 +13,23 @@ while is_valid == False:
 	client_nb = input("> ")
 	if client_nb == "1":
 		is_valid = True
-		port_nb = 60002
+		port_list = 50000
+		port_conn = 60000
 
 	elif client_nb == "2":
 		is_valid = True
-		port_nb = 60003
+		port_list = 50001
+		port_conn = 60001
 
 	else:
 		print("Commande invalide, veuillez réesayer.\n")
 		is_valid = False
 
 print("Creation du websocket...\n")
-client_socket = socket_comm("127.0.0.1", port_nb)
-client_socket.listen(5)
-time.sleep(3)
-client_socket.connect("127.0.0.1", 60001)
-
-print("Creation du client HTTP...\n")
-client = http_client("127.0.0.1", port_nb, "127.0.0.1", 60000, 5)
-header = {"Content-type": "text/plain"}
-
-(error, resp) = client.request('GET', '/', header, '')
-
-if(error == 0):
-	if(resp == b'server : OK'):
-		print("Le serveur est disponible:\n")
-	else:
-		print("Le serveur n'est pas disponible.\n")
-else:
-	print("La requête a échouée, code error : " + str(error) + '\n')
-
-valid_username = False
-while (valid_username == False):
-	print("Veuillez entrer votre identifiant:\n")
-	body = input("> ")
-	(error, resp) = client.request('POST', '/username', header, body)
-
-	if(error == 0):
-		if(resp == b'username : OK'):
-			valid_username = True
-		else:
-			print("Votre identifiant est incorrect.\n")
-	else:
-		print("La requête a échouée, code error : " + str(error) + '\n')
-
-valid_password = False
-while valid_password == False:
-	print("Veuillez entrer votre mots de passe:\n")
-	body = input("> ")
-	(error, resp) = client.request('POST', '/password', header, body)
-
-	if(error == 0):
-		if(resp == b'password : OK'):
-			valid_password = True
-		else:
-			print("Votre mots de passe est incorrect.\n")
-	else:
-		print("La requête a échouée, code error : " + str(error) + '\n')
-
-print("\nFélicitation, vous êtes connecté sur le meilleur chat des EISE5 !\n")
-print("Veuillez tapez join pour rejoindre un salon, create pour creer un nouveau salon ou bien quit pour fermer l'application :\n")
-
-is_valid 	= False
-is_joining 	= False
-is_creating = False
-
-## CHOICE ACTION LOOP
-
-while (is_valid == False):
-
-	choice = input("> ")
-
-	if choice == 'quit':
-		is_valid = True
-		print("\nNous sommes triste de vous voir quittez le meilleur chat des EISE5 !\n")
-		exit()
-
-	elif choice == 'join':
-		is_valid = True
-		
-		(error, resp) = client.request('GET', '/room', header, '')
-
-		if(error == 0):
-			resp_str = resp.decode("utf-8")
-			
-			print("Liste des salons disponible :\n\n")
-			print(resp_str)
-
-			is_joining = True
-		else:
-			print("La requête a échouée, code error : " + str(error) + '\n')
-
-	elif choice == 'create':
-		is_valid = True
-
-		print("Vous allez maintenant créer un nouveau salon.")
-		is_creating = True
-
-	else:
-		print("Commande invalide, veuillez réesayer.")
-		is_valid = False
-
-## JOIN ROOM CHOICE LOOP
-if is_joining == True:
-
-	room_exist == False
-	while room_exist == False:
-
-		print("Veuillez taper le nom du salon que vous souhaitez rejoindre.")
-		chan = input("> ")
-
-		(error, resp) = client.request('POST', '/room', header, chan)
-		if(error == 0):
-			if(resp == b'room : OK'):
-				print("Le salon " + chan + " est disponible.\n")
-				room_exist = True
-			else:
-				print("Le salon " + chan + " n'est pas disponible ou n'existe pas.\n")
-				room_exist = False
-		else:
-			print("La requête a échouée, code error : " + str(error) + '\n')
-			room_exist = False
-
-
-elif is_creating == True:
-
-	room_create = False
-	while room_create == False:
-
-		print("Veuillez taper le nom du salon que vous souhaitez créer.")
-		chan = input("> ")
-
-		(error, resp) = client.request('POST', '/create', header, chan)
-		if(error == 0):
-			if(resp == b'create : OK'):
-				print("Le salon " + chan + " est créé.\n")
-				room_create= True
-			else:
-				print("Vous ne pouvez pas créer le salon " + chan + ".\n")
-				room_create = False
-		else:
-			print("La requête a échouée, code error : " + str(error) + '\n')
-			room_exist = False
-
-print("Bienvenue sur le salon " + chan + " !")
-print("(taper quit pour fermer l'application)\n")
+client_socket = socket_comm()
+print(client_socket.listen("127.0.0.1", port_list, 5))
+time.sleep(5)
+print(client_socket.connect("127.0.0.1", port_conn))
 
 print("\nVeuillez entrer votre message :")
 
@@ -174,3 +46,7 @@ while (True):
 	client_socket.send_message(mes)
 	time.sleep(1)
 
+command = 'kill -9 $(lsof -t -i tcp:50000)'
+os.system(command)
+command = 'kill -9 $(lsof -t -i tcp:50001)'
+os.system(command)
--- http_sql_socket/script_test_server.py
+++ http_sql_socket/script_test_server.py
@@ -1,24 +1,24 @@
 # script for lunching sever
 
-import time
+import time, os
 
 from http_server import http_server
 from socket_comm import socket_comm
 
 if __name__ == '__main__':
 
-	print("Creation du Serveur HTTP...\n")
-	serv_test = http_server("127.0.0.1", 60000)
-
 	print("Creation du websocket...\n")
-	server_socket = socket_comm("127.0.0.1", 60001)
-	server_socket.listen(5)
-	time.sleep(3)
-	server_socket.connect("127.0.0.1", 60002)
-	# server_socket.connect("127.0.0.1", 60003)
+	server_socket = socket_comm()
+	print(server_socket.listen("127.0.0.1", 60000, 5))
+	print(server_socket.listen("127.0.0.1", 60001, 5))
+	time.sleep(5)
+	print(server_socket.connect("127.0.0.1", 50000))
+	print(server_socket.connect("127.0.0.1", 50001))
 
-	print("Lancement du Serveur HTTP...\n")
-	serv_test.run()
-
-	time.sleep(2)
+	time.sleep(20)
 	server_socket.stop_threads = True
+
+	command = 'kill -9 $(lsof -t -i tcp:60000)'
+	os.system(command)
+	command = 'kill -9 $(lsof -t -i tcp:60001)'
+	os.system(command)
