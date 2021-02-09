da243767611eb1e3b573ad3bab783a3fdd4f6f99
azizidomar <azizidomar@etu.upmc.fr>
2021-02-09 12:24:12 +0100

    update-script_test_server-adding_kill_tcp_port

--- socket_comm/script_test_server.py
+++ socket_comm/script_test_server.py
@@ -2,10 +2,10 @@ from socket_comm import socket_comm
 import time
 
 server_socket = socket_comm()
-server_socket.listen("127.0.0.1", 60001, 5)
-time.sleep(1)
+server_socket.listen("127.0.0.1", 60000, 5)
+time.sleep(3)
 
-server_socket.connect("127.0.0.1", 60000)
+server_socket.connect("127.0.0.1", 60001)
 time.sleep(.5)
 
 server_socket.send_message("Hello client!")
@@ -13,5 +13,10 @@ time.sleep(.5)
 
 server_socket.send_message("Hello again client!")
 time.sleep(.5)
+
+# kill the test client
+command = 'kill -9 $(lsof -t -i tcp:60000)'
+os.system(command)
+time.sleep(1)
 
 server_socket.stop_threads = True
